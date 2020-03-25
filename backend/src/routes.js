a7bde906f3ac9403c1696afd603b0b97dc3fb92a
Helder Burato Berto <helder.burato@gmail.com>
2020-03-25 20:27:44 -0300

    backend(feat): add ProfileController with index

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 // ONGS
 routes.get('/ongs', OngController.index);
@@ -11,5 +12,8 @@ routes.post('/ongs', OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+// PROFILE
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
