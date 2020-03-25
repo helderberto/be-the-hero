923f1e964a25b7b8f3a8b8533fd30094688e2034
Helder Burato Berto <helder.burato@gmail.com>
2020-03-25 20:23:01 -0300

    backend(feat): add IncidentController with create, index, delete operations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,15 @@
 const routes = require('express').Router();
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
+// ONGS
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+// INCIDENTS
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
