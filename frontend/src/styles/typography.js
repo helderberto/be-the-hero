3b0fd13aaf14d6c4fb8f88064e903418693de239
Helder Burato Berto <helder.burato@gmail.com>
2020-03-28 15:25:29 -0300

    frontend(feat): add styles typography to share in app

---
+++ frontend/src/styles/typography.js
@@ -0,0 +1,16 @@
+const Typography = {
+  // FONT FAMILY
+  primaryFamily: 'Roboto, sans-serif',
+
+  // FONT WEIGHT
+  weightRegular: 400,
+  weightMedium: 500,
+  weightBold: 700,
+
+  // FONT SIZE
+  primarySize: '1.4rem',
+  size18: '1.8rem',
+  size16: '1.6rem'
+};
+
+export default Typography;
