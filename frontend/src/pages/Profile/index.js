3f1274db3eddee2deac10bfa4978aebab8c2b5ed
Helder Burato Berto <helder.burato@gmail.com>
2020-03-30 18:54:24 -0300

    frontend(feat): add page Profile

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
+
+import logoImg from 'assets/logo.svg';
+
+export default function Profile() {
+  return (
+    <div>
+      <header>
+        <img src={logoImg} alt="Be The Hero" />
+        <span>Bem vindo(a), APAD</span>
+
+        <Link to="/incidents/new">Cadastrar novo caso</Link>
+
+        <button type="button">
+          <FiPower size={18} color="#E02041" />
+        </button>
+      </header>
+    </div>
+  );
+}
