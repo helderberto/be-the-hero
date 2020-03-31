5c25703c90c27098f4e8ebdb722afb55b4f8b590
Helder Burato Berto <helder.burato@gmail.com>
2020-03-30 22:52:10 -0300

    frontend(feat): add IconTrash component

---
+++ frontend/src/components/atoms/Icons/IconTrash/styled.js
@@ -0,0 +1,8 @@
+import { FiTrash2 } from 'react-icons/fi';
+import styled from 'styled-components';
+
+import { commonStyle } from '../common';
+
+export const IconTrash = styled(FiTrash2)`
+  ${commonStyle}
+`;
