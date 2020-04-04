fc1195de600c444f6d2c5bed9b2d47c7ee8649ee
Helder Burato Berto <helder.burato@gmail.com>
2020-04-04 17:39:02 -0300

    frontend(refactor): update button destructuring from theme to use fontWeights and colors

--- frontend/src/components/atoms/Button/styled.js
+++ frontend/src/components/atoms/Button/styled.js
@@ -1,21 +1,23 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  width: 100%;
-  height: 60px;
-  background: ${({ theme }) => theme.colors.red};
-  border: 0;
-  border-radius: 8px;
-  color: ${({ theme }) => theme.colors.white};
-  font-weight: ${({ theme }) => theme.fontWeights.bold};
-  margin-top: 16px;
-  display: inline-block;
-  text-align: center;
-  text-decoration: none;
-  line-height: 60px;
-  transition: filter 0.2s;
+  ${({ theme: { fontWeights, colors } }) => `
+    width: 100%;
+    height: 60px;
+    background: ${colors.red};
+    border: 0;
+    border-radius: 8px;
+    color: ${colors.white};
+    font-weight: ${fontWeights.bold};
+    margin-top: 16px;
+    display: inline-block;
+    text-align: center;
+    text-decoration: none;
+    line-height: 60px;
+    transition: filter 0.2s;
 
-  &:hover {
-    filter: brightness(90%);
-  }
+    &:hover {
+      filter: brightness(90%);
+    }
+  `}
 `;
