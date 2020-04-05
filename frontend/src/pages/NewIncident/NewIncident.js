63fd282bcb012ef5a57b60796edc516345946356
Helder Burato Berto <helder.burato@gmail.com>
2020-04-04 23:02:45 -0300

    frontend(feat): add NewIncident create incidents

--- frontend/src/pages/NewIncident/NewIncident.js
+++ frontend/src/pages/NewIncident/NewIncident.js
@@ -2,8 +2,40 @@ import React from 'react';
 import * as S from './styled';
 import { IconLink, Button } from 'components/atoms';
 import logoImg from 'assets/logo.svg';
+import { useForm } from 'hooks';
+import { Http } from 'interfaces';
+import { useHistory } from 'react-router-dom';
+
+const INITIAL_FORM_STATE = {
+  title: '',
+  description: '',
+  value: '',
+};
 
 function NewIncident() {
+  const [values, resetForm, handleChange] = useForm(INITIAL_FORM_STATE);
+  const ongId = localStorage.getItem('ongId');
+  const history = useHistory();
+
+  async function handleCreateIncident(evt) {
+    evt.preventDefault();
+
+    try {
+      await Http.post('incidents', values, {
+        headers: {
+          Authorization: ongId,
+        },
+      });
+
+      alert('Incidente cadastrado com sucesso! :D.');
+      resetForm();
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Não foi possível cadastrar o incidente.');
+    }
+  }
+
   return (
     <S.Container>
       <S.Content>
@@ -22,10 +54,26 @@ function NewIncident() {
           </IconLink>
         </S.Section>
 
-        <S.Form>
-          <S.Input placeholder="Título do caso" />
-          <S.Input as="textarea" placeholder="Descrição" />
-          <S.Input placeholder="Valor em Reais" />
+        <S.Form onSubmit={handleCreateIncident}>
+          <S.Input
+            name="title"
+            value={values.title}
+            placeholder="Título do caso"
+            onChange={handleChange}
+          />
+          <S.Input
+            as="textarea"
+            name="description"
+            value={values.description}
+            placeholder="Descrição"
+            onChange={handleChange}
+          />
+          <S.Input
+            name="value"
+            value={values.value}
+            placeholder="Valor em Reais"
+            onChange={handleChange}
+          />
 
           <Button type="submit">Cadastrar</Button>
         </S.Form>
