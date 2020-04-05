8be028baf8d55c608d2dd77f676eaab13b05d536
Helder Burato Berto <helder.burato@gmail.com>
2020-04-05 18:18:28 -0300

    frontend(refactor): update methods from useForm to way of useReducer

--- frontend/src/hooks/useForm.js
+++ frontend/src/hooks/useForm.js
@@ -1,23 +1,52 @@
 import { useReducer } from 'react';
 import { mergeObject } from 'utils';
 
+const Types = {
+  UPDATE: 'useForm/update',
+  RESET: 'useForm/reset',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case Types.UPDATE: {
+      return mergeObject(state, { [action.name]: action.value });
+    }
+
+    case Types.RESET: {
+      return action.initialState;
+    }
+
+    default: {
+      throw Error('🚫 Required initial state to start the useForm hook.');
+    }
+  }
+}
+
 function useForm(initialState = {}) {
-  const [values, setValues] = useReducer(
-    (state, newState) => mergeObject(state, newState),
-    initialState
-  );
+  const [values, dispatch] = useReducer(reducer, initialState);
 
   function handleChange(evt) {
     const { name, value } = evt.target;
 
-    setValues({ [name]: value });
+    return dispatch({
+      type: Types.UPDATE,
+      name,
+      value,
+    });
   }
 
   function resetForm() {
-    setValues(initialState);
+    return dispatch({
+      type: Types.RESET,
+      initialState,
+    });
   }
 
-  return [values, resetForm, handleChange];
+  return {
+    values,
+    handleChange,
+    resetForm,
+  };
 }
 
 export { useForm };
